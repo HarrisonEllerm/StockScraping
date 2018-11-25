9492e121d5e72b0a47a4bd03982e9a8d19339965
Harry Ellerm <harryellerm@gmail.com>
2018-11-25 14:44:06 +1300

    Added primitive exception handling and small cli for testing

--- scraper.py
+++ scraper.py
@@ -11,11 +11,14 @@ from bs4 import BeautifulSoup
     Author: Harry Ellerm
 """
 
+summary_exception_string = 'No summary information could be found for the ticker. URL was: {}'
+stats_exception_string = 'No statistical information could be found for the ticker. URL was: {}'
+
 
 def get_page(url):
     """
-    Accepts a single url argument and makes a GET
-    request to that url.
+        Accepts a single url argument and makes a GET
+        request to that url.
     """
     try:
         # closing function makes sure that any network
@@ -32,16 +35,46 @@ def get_page(url):
         return None
 
 
+def get_summary(url):
+    """
+        Gets the summary info for a particular stock by parsing the
+        corresponding Yahoo Finance web page.
+        :param url: the url
+        :return: an array of key value pairs representing the stats found,
+        if any are found.
+    """
+    response = get_page(url)
+    if response is not None:
+        soup = BeautifulSoup(response, 'html.parser')
+        summary_items = []
+        div = soup.find('div', {'id': 'quote-summary'})
+        if div is not None:
+            tables_in_div = div.findAll('table')
+            for table in tables_in_div:
+                table_data = table.findAll('tr')
+                for row in table_data:
+                    cells = row.findAll('td')
+                    statistic = cells[0].find(text=True), cells[1].find(text=True)
+                    summary_items.append(statistic)
+        else:
+            raise Exception(summary_exception_string.format(url))
+
+        if len(summary_items) != 0:
+            return summary_items
+        else:
+            raise Exception(summary_exception_string.format(url))
+
+
 def get_stats(url, search_terms):
     """
-    Gets the statistics for a particular stock by parsing the
-    corresponding Yahoo Finance web page.
-    :param url: the url
-    :param search_terms: the terms corresponding to the statistics
-    the user is interested in, i.e. Valuation measures, Fiscal year,
-    Profitability etc.
-    :return: an array of key value pairs representing the stats found,
-    or None if no stats could be found.
+        Gets the statistics for a particular stock by parsing the
+        corresponding Yahoo Finance web page.
+        :param url: the url
+        :param search_terms: the terms corresponding to the statistics
+        the user is interested in, i.e. Valuation measures, Fiscal year,
+        Profitability etc.
+        :return: an array of key value pairs representing the stats found,
+        if any are found.
     """
     response = get_page(url)
     if response is not None:
@@ -49,31 +82,37 @@ def get_stats(url, search_terms):
         stats = []
         for item in search_terms:
             table = get_table_corresponding_to_search_term(soup, item)
-            table_data = table.findAll('tr')
-            for row in table_data:
-                cells = row.findAll('td')
-                statistic = cells[0].find(text=True), cells[1].find(text=True)
-                stats.append(statistic)
+            if table is not None:
+                table_data = table.findAll('tr')
+                for row in table_data:
+                    cells = row.findAll('td')
+                    statistic = cells[0].find(text=True), cells[1].find(text=True)
+                    # remove all N/A stats
+                    if 'N/A' not in statistic[1]:
+                        stats.append(statistic)
 
         if len(stats) != 0:
             return stats
         else:
-            return None
+            raise Exception(stats_exception_string.format(url))
 
 
 def get_table_corresponding_to_search_term(soup, search_term):
     """
-    Returns a table from within the HTML, specified by
-    a search term.
-    :param soup: the soup object
-    :param search_term: the search term
-    :return: a table corresponding to the search term
-    if found, or None if no table is found.
+        Returns a table from within the HTML, specified by
+        a search term.
+        :param soup: the soup object
+        :param search_term: the search term
+        :return: a table corresponding to the search term
+        if found, or None if no table is found.
     """
-    # first parent steps out to span
-    # second parent steps out of span to div
-    str_val = soup.find(text=search_term).parent.parent
-    table = str_val.findNext("table")
+    str_val = soup.find(text=search_term)
+    if str_val is not None:
+        # first parent steps out to span
+        # second parent steps out of span to div
+        table = str_val.parent.parent.findNext('table')
+    else:
+        return None
 
     if len(table) != 0:
         return table
@@ -83,8 +122,8 @@ def get_table_corresponding_to_search_term(soup, search_term):
 
 def is_good(resp):
     """
-    Defines if a request is valid by analysing the
-    content-type header of the HTML, if any is returned.
+        Defines if a request is valid by analysing the
+        content-type header of the HTML, if any is returned.
     """
     content_type = resp.headers['Content-Type'].lower()
     return (resp.status_code == 200
@@ -94,8 +133,6 @@ def is_good(resp):
 
 def log_exception(e):
     """
-    Logs an exception thrown during execution.
+        Logs an exception thrown during execution.
     """
     print(e)
-
-
--- testscraper.py
+++ testscraper.py
@@ -1,14 +1,58 @@
 from scraper import get_stats
+from scraper import get_summary
 
-search_terms = ["Valuation measures", "Fiscal year", "Profitability", "Management effectiveness", "Income statement",
-                "Balance sheet", "Cash flow statement", "Stock price history", "Share statistics", "Dividends & splits"]
+stats_search_terms = ["Valuation measures",
+                "Fiscal year",
+                "Profitability",
+                "Management effectiveness",
+                "Income statement",
+                "Balance sheet",
+                "Cash flow statement",
+                "Stock price history",
+                "Share statistics",
+                "Dividends & splits"]
 
-result = get_stats('https://nz.finance.yahoo.com/quote/FBU.NZ/key-statistics?p=FBU.NZ', search_terms)
+while True:
 
-for item in result:
-    print 'key: ' + item[0] + ', value: ' + item[1]
+    symbol = raw_input("Please enter ticker symbol and option, or help to find out more information.\n")
 
+    if symbol.upper().__contains__("QUIT"):
+        break
 
+    elif symbol.upper().__contains__("HELP"):
+        print("Possible options:")
+        print(" -sum, usage: [FBU.NZ --sum], finds summary information associated with the ticker.")
+        print(" -stats, usage: [FBU.NZ --stats], finds statistics associated with the ticker.")
+        print(" help, usage: [help], prints this menu.")
+        print(" quit, usage: [quit], quits the program.\n")
 
+    # handle the summary report case
+    elif "sum" in symbol:
+        try:
 
+            summary_items = get_summary('https://nz.finance.yahoo.com/quote/' + symbol.upper().replace(' -SUM', '') +
+                                        '?p=' + symbol.upper().replace(' -SUM', ''))
+            print "****** SUMMARY INFO ******"
+            for item in summary_items:
+                print item[0] + ' = ' + item[1]
+            print("**************************\n")
 
+        except Exception as error:
+            print error
+
+    # handle the statistics case
+    elif "stats" in symbol:
+        try:
+
+            stats = get_stats('https://nz.finance.yahoo.com/quote/' + symbol.upper().replace(' -STATS', '') +
+                              '/key-statistics?p=' + symbol.upper().replace(' -STATS', ''), stats_search_terms)
+            print "******  STATISTICS  ******"
+            for item in stats:
+                print item[0] + ' = ' + item[1]
+            print("**************************\n")
+
+        except Exception as error:
+            print error
+
+    else:
+        print("Command unknown, use 'help' to display a list of possible commands.\n")
